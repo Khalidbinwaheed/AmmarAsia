44aa676e146495ac2be7320f1a50a7302dd21716
Khalid Bin Waheed <captainkhalid20@gmail.com>
2026-02-03 21:38:00 +0500

    .

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import Lenis from 'lenis';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import WhatsAppButton from '../UI/WhatsAppButton';
@@ -16,6 +17,29 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+            orientation: 'vertical',
+            gestureOrientation: 'vertical',
+            smoothWheel: true,
+            wheelMultiplier: 1,
+            touchMultiplier: 2,
+        });
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
     return (
         <>
             <Navbar />
